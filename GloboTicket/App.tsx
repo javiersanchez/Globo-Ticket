65fa174e3bf9b519f297cf0d239bacea34ee956e
Javier <jsanchez@occ.com.mx>
2023-05-16 09:39:00 -0600

    Initial Commit

---
+++ GloboTicket/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
+import Home from './Home';
+
+function App(): JSX.Element {
+
+  return (
+    <Home username='Sports fan' />
+  );
+}
+
+export default App;
